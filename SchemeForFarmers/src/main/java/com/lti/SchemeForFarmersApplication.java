5202bebbb5bda7038ff24c4a684b1f86d7f20fde
kaviya72 <kaviyasrini1999@gmail.com>
2021-01-11 11:05:43 +0530

    ignoree

--- SchemeForFarmers/src/main/java/com/lti/SchemeForFarmersApplication.java
+++ SchemeForFarmers/src/main/java/com/lti/SchemeForFarmersApplication.java
@@ -8,6 +8,7 @@ public class SchemeForFarmersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchemeForFarmersApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
