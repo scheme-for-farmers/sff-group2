5395d0e00a8c6419590d5753dac9490749277901
SowmyaS98 <sowmyselvaraj.ssk@gmail.com>
2021-01-18 14:16:25 +0530

    transactional commit

--- SchemeForFarmers/src/main/java/com/lti/repository/FarmerRepositoryImpl.java
+++ SchemeForFarmers/src/main/java/com/lti/repository/FarmerRepositoryImpl.java
@@ -6,9 +6,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-import javax.transaction.Transactional;
+
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lti.entity.Farmer;
 
